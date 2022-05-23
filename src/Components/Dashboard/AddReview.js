5f2b916c3229dd6da5b7d7f4fe83faee67e4b6b5
rahimuj570 <rahimuj570@gmail.com>
2022-05-23 19:23:23 +0600

    rev Ui added

--- src/Components/Dashboard/AddReview.js
+++ src/Components/Dashboard/AddReview.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.int";
 
 const AddReview = () => {
-  return <div></div>;
+  const [user] = useAuthState(auth);
+  return (
+    <>
+      <div className="text-center mb-10 border-b-4 md:w-3/6 w-5/6 pb-1 mx-auto text-3xl font-bold">
+        Add Reviews
+      </div>
+
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const revData = { name: user.displayName, rev: e.target.rev.value };
+          console.log(revData);
+        }}
+        className="text-center w-full"
+      >
+        <textarea
+          required
+          placeholder="Write Here..."
+          className="w-5/6 mx-auto bg-slate-50 p-2"
+          name="rev"
+        ></textarea>{" "}
+        <br />
+        <button className="btn text-white mt-2 btn-primary"> Add Review</button>
+      </form>
+    </>
+  );
 };
 
 export default AddReview;
